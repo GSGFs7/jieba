2230efbb709be50c97c6f66e3e28d5c1ac7ecbda
GSGFs <gsgfs@gsgfs.moe>
2025-11-23 23:29:50 +0800

    fix: replace deprecated API pkg_resources

--- jieba/_compat.py
+++ jieba/_compat.py
@@ -12,16 +12,17 @@ def setLogLevel(log_level):
     default_logger.setLevel(log_level)
 
 
-check_paddle_install = {'is_paddle_installed': False}
+check_paddle_install = {"is_paddle_installed": False}
 
 try:
-    import pkg_resources
+    from importlib.resources import files
 
-    get_module_res = lambda *res: pkg_resources.resource_stream(__name__,
-                                                                os.path.join(*res))
+    get_module_res = lambda *res: files(__name__.split(".")[0]).joinpath(*res)
 except ImportError:
-    get_module_res = lambda *res: open(os.path.normpath(os.path.join(
-        os.getcwd(), os.path.dirname(__file__), *res)), 'rb')
+    get_module_res = lambda *res: open(
+        os.path.normpath(os.path.join(os.getcwd(), os.path.dirname(__file__), *res)),
+        "rb",
+    )
 
 
 def enable_paddle():
@@ -35,50 +36,45 @@ def enable_paddle():
         except ImportError:
             default_logger.debug(
                 "Import paddle error, please use command to install: pip install paddlepaddle-tiny==1.6.1."
-                "Now, back to jieba basic cut......")
-    if paddle.__version__ < '1.6.1':
-        default_logger.debug("Find your own paddle version doesn't satisfy the minimum requirement (1.6.1), "
-                             "please install paddle tiny by 'pip install --upgrade paddlepaddle-tiny', "
-                             "or upgrade paddle full version by "
-                             "'pip install --upgrade paddlepaddle (-gpu for GPU version)' ")
+                "Now, back to jieba basic cut......"
+            )
+    if paddle.__version__ < "1.6.1":
+        default_logger.debug(
+            "Find your own paddle version doesn't satisfy the minimum requirement (1.6.1), "
+            "please install paddle tiny by 'pip install --upgrade paddlepaddle-tiny', "
+            "or upgrade paddle full version by "
+            "'pip install --upgrade paddlepaddle (-gpu for GPU version)' "
+        )
     else:
         try:
             import jieba.lac_small.predict as predict
+
             default_logger.debug("Paddle enabled successfully......")
-            check_paddle_install['is_paddle_installed'] = True
+            check_paddle_install["is_paddle_installed"] = True
         except ImportError:
-            default_logger.debug("Import error, cannot find paddle.fluid and jieba.lac_small.predict module. "
-                                 "Now, back to jieba basic cut......")
+            default_logger.debug(
+                "Import error, cannot find paddle.fluid and jieba.lac_small.predict module. "
+                "Now, back to jieba basic cut......"
+            )
 
-
-PY2 = sys.version_info[0] == 2
 
 default_encoding = sys.getfilesystemencoding()
 
-if PY2:
-    text_type = unicode
-    string_types = (str, unicode)
+text_type = str
+string_types = (str,)
+xrange = range
 
-    iterkeys = lambda d: d.iterkeys()
-    itervalues = lambda d: d.itervalues()
-    iteritems = lambda d: d.iteritems()
-
-else:
-    text_type = str
-    string_types = (str,)
-    xrange = range
-
-    iterkeys = lambda d: iter(d.keys())
-    itervalues = lambda d: iter(d.values())
-    iteritems = lambda d: iter(d.items())
+iterkeys = lambda d: iter(d.keys())
+itervalues = lambda d: iter(d.values())
+iteritems = lambda d: iter(d.items())
 
 
 def strdecode(sentence):
     if not isinstance(sentence, text_type):
         try:
-            sentence = sentence.decode('utf-8')
+            sentence = sentence.decode("utf-8")
         except UnicodeDecodeError:
-            sentence = sentence.decode('gbk', 'ignore')
+            sentence = sentence.decode("gbk", "ignore")
     return sentence
 
 
